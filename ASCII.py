6696ce015a41784404782447ce1831113a100165
Ralph <rm00457@rvt.lv>
2023-12-13 21:16:05 +0200

    Added instructions to main 13.12.2023.

---
+++ ASCII.py
@@ -0,0 +1,19 @@
+# Art by Ralph
+
+def showArt():
+    art = '''
+    Hello! Introducing SmartPiano v.1
+
+    Here you will try out all the keys given in the following:
+
+    - DRUM
+    1 2 3 4 5 6 7 8 9
+
+    - SYNTHESIZER
+    # short keys        # long keys         # electronic keys
+    q w e r             a s d f             z x c v
+
+    - GUITAR
+    h u j k l
+    '''
+    print(art)
--- main.py
+++ main.py
@@ -3,10 +3,16 @@
 from drumpad import drum_pad
 from synthesizer import synthesizer_pad
 from guitarpad import guitar_pad
+from ASCII import showArt
 from threading import Thread
+from os import system
 
+system("cls")   # to clear previous text
 
 def main():
+
+    showArt()
+
     thread1 = Thread(target=drum_pad)
     thread2 = Thread(target=synthesizer_pad)
     thread3 = Thread(target=guitar_pad)
